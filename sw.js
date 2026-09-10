82b3f95bbbf035089013493d874cf4e38958a1f6
RODRIGO ALEXANDRE LUCAS ALVES <rodrigo.lucas.alves@tecnico.ulisboa.pt>
2026-09-10 19:47:22 +0100

    updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version any time you update the app files, so old caches get replaced.
-var CACHE_NAME = "n5-flashcards-v2";
+var CACHE_NAME = "n5-flashcards-v3";
 var ASSETS = [
   "./",
   "./index.html",
